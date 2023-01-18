1115bb60121b35f3b25daddea501c829f010316c
romain-garcia-rodriguez <romain.garcia@hotmail.com>
2023-01-18 19:30:35 +0100

    Init project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup></script>
+
+<template>
+  <!-- Exercice 4.5
+
+  - Créer un champ input avec un listener qui affiche le contenu entré
+
+  - Créer un composant "Character" qui va contenir chaque lettre dans un
+  <button> (Un composant par lettre) les unes à coté des autres dans des composants
+    distincts (html final : <button>B</button> <button>o</button><button>n</button>).
+
+  - Lorsque l'utilisateur clique sur un bouton d'un composant Character, cela va
+  supprimer la lettre du mot (dans l'affichage et dans l'input)
+
+  CSS de l'exemple :
+
+  .char{
+          display: 'inline-block',
+          padding: '16px',
+          margin: '16px',
+          border: '1px solid black',
+          text-align: 'center'
+      }; -->
+  <main></main>
+</template>
+
+<style scoped></style>
